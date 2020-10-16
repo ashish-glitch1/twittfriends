a5f3d2e6edec796c67a81d86ad10fe79af5d2df0
ashish-glitch1 <72959735+ashish-glitch1@users.noreply.github.com>
2020-10-16 19:53:39 +0530

    Add files via upload

---
+++ hidden1.py
@@ -0,0 +1,9 @@
+#keep this file separate and enter your generated key and secret in respective fields
+# get a developer account on https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "************",
+            "consumer_secret": "************",
+            "token_key": "*************",
+            "token_secret": "*************"}
